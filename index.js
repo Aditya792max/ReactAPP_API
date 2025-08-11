6b4c833010ac99aeaf052709ced1cec171b0d759
Aditya Vikram KIrtania <108626210+Aditya792max@users.noreply.github.com>
2025-08-11 23:17:06 +0530

    Branch_1
    By the help of this branch we are
    going to work on All the sub-types of the REST APIs.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.listen(PORT, () => {
      console.log("Server has started");
      console.log(`Server is running on port ${PORT}`);
 });
-
+// This is the POST API calling 
 app.post("/post", async (req, res) => {
      console.log(req.body);
      // res.status(200).json({ message: "Data received" });
@@ -20,10 +20,18 @@ app.post("/post", async (req, res) => {
      //      res.send({ status: "Unknown User" });
      // }
      // res.send({ status: "Message Received" });
+
+     const My_data = {
+          "name": "Aditya Vikram Kirtania",
+          "age": 25,
+          "location": "India"
+     };
+
      try {
           const { data } = req.body;
-          if (data === "aditya") {
+          if (JSON.stringify(data) === JSON.stringify(My_data.name)) {
                res.send({ status: "Message Received" });
+
           } else {
                res.send({ status: "Unknown User" });
           }
@@ -32,4 +40,21 @@ app.post("/post", async (req, res) => {
           res.status(500).send({ status: "Something Went Wrong" });
      }
 
+});
+
+// This is the GET API calling.
+
+app.get("/get", async (req, res) => {
+     try {
+          // Simulate fetching data
+          const data = {
+               "name": "Aditya Vikram Kirtania",
+               "age": 25,
+               "location": "India"
+          }
+          res.status(200).json(data);
+     } catch (error) {
+          console.error("Error:", error);
+          res.status(500).send({ status: "Something Went Wrong" });
+     }
 });
